85f2b4f91a62f2f107cb1f0e5c111b14c925a367
LappyTon <ganailyu4ok@gmail.com>
2024-05-03 15:15:46 +0300

    front-end: password and e-mail validator

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ email = document.getElementById("login_email"),
 login1 = document.getElementById('login1'),
 login2 = document.getElementById('login2'),
 login3 = document.getElementById('login3'),
+login4 = document.getElementById('login4'),
+login5 = document.getElementById('login5'),
+login6 = document.getElementById('login6'),
 
 buttonWorking = document.getElementById('btnworking'),
 buttonNotWorking = document.getElementById('btnnotworking');
@@ -23,36 +26,16 @@ const emailDomains = [
     'yandex.com',
     'fastmail.com'
 ];
+const regex = /^[a-zA-Z0-9_\-.,]+$/;
 
-const commonPasswords = [
-    "123456", "password", "123456789", "12345678", "12345", 
-    "1234567", "1234567890", "123123", "password1", "abc123", 
-    "1234", "qwerty", "123456a", "123", "iloveyou", 
-    "12345678910", "welcome", "monkey", "password123", "123456789a", 
-    "123456789b", "123456789c", "admin", "passw0rd", "password!",
-    "123456789d", "123456789e", "login", "pass123", "letmein", 
-    "password12", "123456789f", "123456789g", "starwars", "123123123", 
-    "test", "password1234", "123456789h", "123456789i", "123456789j", 
-    "123abc", "123456789k", "987654321", "9876543211", "123456789m", "123456789n", 
-    "password2", "password12345", "123456789o", "123456789p", "password123456",
-    "123456789q", "123456789r", "password1234", "123456789s", "123456789t", 
-    "123456789u", "123456789v", "qwerty123", "password123456789", "123456789w", 
-    "123456789x", "password123!", "123456789y", "123456789z", "qwertyuiop", 
-    "1234567890a", "1234567890b", "password1234567", "1234567890c", "1234567890d", 
-    "1234567890e", "1234567890f", "password12345678", "1234567890g", "1234567890h", 
-    "1234567890i", "password1234567890", "1234567890j", "1234567890k", "1234567890l", 
-    "1234567890m", "1234567890n", "1234567890o", "1234567890p", "1234567890q", 
-    "1234567890r", "1234567890s", "1234567890t", "1234567890u", "1234567890v", 
-    "1234567890w", "1234567890x", "1234567890y", "1234567890z", "12345678900", 
-    "12345678901", "12345678902", "12345678903", "12345678904", "12345678905", 
-    "12345678906", "12345678907", "12345678908", "12345678909", "123456789010", 
-];
+
 
 function validatePassword() {
-    const value = password.value.trim();
+    const value = password.value;
     if (value === "") return;
-
-    login1.style.display = commonPasswords.includes(value) ? 'flex' : 'none';
+    
+    login1.style.display = value.indexOf(' ')>=0 ? 'flex' : 'none';
+    login6.style.display = regex.test(value) ? 'none' : 'flex';
     login4.style.display = value.length < 8 ? 'flex' : 'none';
     login5.style.display = value.length > 100 ? 'flex' : 'none';
 }
