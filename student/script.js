ae447faa822f801e0b17615b3c1d34ca26986b6b
LappyTon <69641782+LappyTon@users.noreply.github.com>
2024-05-18 00:44:06 +0300

    Add files via upload

--- student/script.js
+++ student/script.js
@@ -1,3 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const userCells = document.querySelectorAll('.sm-subject_cell');
+    const mmactive = document.querySelectorAll('.mm-activity_btn')
+    userCells.forEach(function(cell) {
+        cell.addEventListener('click', function() {
+            userCells.forEach(function(cell) {
+                cell.classList.remove('active');
+            });
+            this.classList.add('active');
+            const marks = document.querySelector(".usermarks"); // Target the first button
+            marks.classList.add('flex'); // Add the 'flex' class
+        });
+    });
+    mmactive.forEach((cell) => {
+        cell.addEventListener('click', function() {
+            mmactive.forEach(function(cell) {
+                cell.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    })
+});
 function truncateText(className, maxHeight) {
     let element = document.querySelector("." + className);
     if (!element) return; 
