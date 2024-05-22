020b0afa8cc762ebf663de818f683c7441437964
Mark Kozhydlo <99179725+kozhydlo@users.noreply.github.com>
2024-05-22 17:00:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./authRouters');
+const Grid = require('gridfs-stream');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use("/auth", authRouter);
+
+const start = async () => {
+    try {
+        const conn = await mongoose.connect(`mongodb+srv://kozhydlomark:mark2010@cluster0.v121azw.mongodb.net/Moodle5?retryWrites=true&w=majority&appName=Cluster0`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        const gfs = Grid(conn.connection.db, mongoose.mongo);
+        gfs.collection('uploads');
+
+        app.set('gfs', gfs); // Збереження gfs в додатку
+
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
